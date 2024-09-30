d0d8948e6bd5c47ef2ff813825d16e619e531321
Ramyrashad127 <ramy41599@gmail.com>
2024-09-30 20:32:36 +0300

    new

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
 // Desc: Create a simple http server
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 });
 
-server.listen(1245);
-module.exports = server;
+app.listen(1245);
+module.exports = app;
