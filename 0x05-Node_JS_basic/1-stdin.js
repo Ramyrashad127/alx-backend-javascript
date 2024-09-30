aa5abf219f49cb7b79efe7d3cd1dfd6357f57267
Ramyrashad127 <ramy41599@gmail.com>
2024-09-30 19:33:42 +0300

    new

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
-process.on('exit', () => {
+process.on('end', () => {
   console.log('This important software is now closing');
 });
