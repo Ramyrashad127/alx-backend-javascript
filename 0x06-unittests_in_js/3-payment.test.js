c052fb56eedefbe408ed5cc6c95649744a90c537
Ramyrashad127 <ramy41599@gmail.com>
2024-10-03 02:55:04 +0300

    new

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+// tests
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi', () => {
+    const obj = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(obj.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(obj.calculateNumber.callCount).to.be.equal(1);
+    obj.calculateNumber.restore();
+  });
+});
