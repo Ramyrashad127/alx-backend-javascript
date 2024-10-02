2182002f390dffd2acda273228825b7e0f3d3377
Ramyrashad127 <ramy41599@gmail.com>
2024-10-03 01:49:04 +0300

    ne

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,56 +2,56 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return 4', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.9, 3), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
-    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
-  });
-  it('rounding up a and b floating point fractional numbers with trailing 9\'s', () => {
-    assert.strictEqual(calculateNumber(2.5, 3.5), 7);
-  });
+describe('calculateNumber', function() {
+    it('should return 4', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.9, 3), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 5', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+    });
+    it('should return 6', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.5), 6)
+    });
+    it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+      assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+    });
 });
