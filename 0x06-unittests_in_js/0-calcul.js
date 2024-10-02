a0642b48ee758b0b0a1ac412547ca779e7d8eaac
Ramyrashad127 <ramy41599@gmail.com>
2024-10-03 01:28:52 +0300

    new

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+// 0-calcul.js
+const my_fun = function calculateNumber(a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+  return a + b;
+};
+module.exports = my_fun;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,51 @@
+// 0-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.9, 3), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+});
