8868864e0be5519e66baefdca9564d556519626f
Ramyrashad127 <ramy41599@gmail.com>
2024-10-04 02:05:52 +0300

    new

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,37 @@
+// api test
+const request = require('request');
+const { expect } = require('chai');
+
+describe('aPI test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('gET /cart/:id', () => new Promise((done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  }));
+
+  it('gET /', () => new Promise((done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+
+  it('gET /cart/:id', () => new Promise((done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  }));
+
+  it('gET /cart/:id', () => new Promise((done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  }));
+});
