c7c249695fc3a4e1d0fb02112c90b88b7cee161f
Ramyrashad127 <ramy41599@gmail.com>
2024-10-03 03:01:21 +0300

    new

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -3,6 +3,6 @@ const fn = require('./utils');
 
 const my_fun = function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const ans = fn.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log('The total is: ', ans);
+  console.log(`The total is: ${ans}`);
 };
 module.exports = my_fun;
